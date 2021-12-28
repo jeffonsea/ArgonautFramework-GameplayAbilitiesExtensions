acc66152fd8d65925ac2a9631d24b75d6d73d9c0
Jeff Onsea <jeffonsea@gmail.com>
2021-12-29 00:28:38 +0100

    Added module.

---
+++ Private/Debug/FAFAbilities_GameplayDebuggerCategory.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright 2022 Argonaut Team
+
+
+#include "Debug/FAFAbilities_GameplayDebuggerCategory.h"
+
+
+FAFAbilities_GameplayDebuggerCategory::FAFAbilities_GameplayDebuggerCategory()
+{
+}
+
+FAFAbilities_GameplayDebuggerCategory::~FAFAbilities_GameplayDebuggerCategory()
+{
+}
---
+++ Public/Debug/FAFAbilities_GameplayDebuggerCategory.h
@@ -0,0 +1,15 @@
+﻿// Copyright 2022 Argonaut Team
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GAMEPLAYABILITIESEXTENSIONS_API FAFAbilities_GameplayDebuggerCategory
+{
+public:
+	FAFAbilities_GameplayDebuggerCategory();
+	~FAFAbilities_GameplayDebuggerCategory();
+};
